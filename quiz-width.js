f3989fe0a13148fc2e20e525d5682f32e4ac33a1
Chase Ramsey <ramsey.chase@gmail.com>
2016-04-19 08:07:50 -0500

    Added form for user to input the character to use to build the tree; some tweaking

--- quiz-width.js
+++ quiz-width.js
@@ -1,21 +1,25 @@
 
 var treeSpecs = {
 	width: 0,
-	char: "*"
+	char: ""
 }
-
-var leaves = [treeSpecs.char];
 
 function tree(clickEvent) {
 	clickEvent.preventDefault();
 	treeSpecs.width = document.getElementById('number').value;
+	treeSpecs.char = document.getElementById('char').value;
+	var leaves = [treeSpecs.char];
+	if (treeSpecs.width === "" || treeSpecs.char === "") {
+		alert('Please enter a value for the height.');
+		return;
+	}
 	for (var i = 0; i < treeSpecs.width; i++) {
 		leaves.unshift(" ");
 	}
-	var count = (leaves.length - 1);
 	console.log(leaves.join(""));
+	var count = (leaves.length - 1 - 1);
 	for (var i = 0; i < count; i++) {
-		leaves.push("*", "*");
+		leaves.push(treeSpecs.char, treeSpecs.char);
 		leaves.shift();
 		console.log(leaves.join(""));
 	}
@@ -23,3 +27,5 @@ function tree(clickEvent) {
 
 var button = document.getElementById('run');
 button.addEventListener("click", tree);
+
+
